dcd8e045efacd796438f9d5b8aebf3f16e80cd92
TAN Minghuan <vimostan@gmail.com>
2021-11-27 16:29:52 +0800

    Update eval

--- docker_eval.sh
+++ docker_eval.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 # Copyright (c) VisualJoyce.
 # Licensed under the MIT license.
 WORK_DIR=$(readlink -f .)
-DATA_DIR=$(readlink -f ${WORK_DIR}/data)
+DATA_DIR=$(readlink -f "${WORK_DIR}"/data)
 CACHE_DIR=${DATA_DIR}/.cache
 ALLENNLP_DIR=${DATA_DIR}/.allennlp
 NLTK_DATA=${DATA_DIR}/nltk_data
@@ -10,12 +11,12 @@ PROJECT=$1
 MODEL_DIR=$2
 RUN_TYPE=$3
 
-docker run --gpus '"'device=$CUDA_VISIBLE_DEVICES'"' --ipc=host --rm -it \
+docker run --gpus '"'device="$CUDA_VISIBLE_DEVICES"'"' --ipc=host --rm -it \
   --mount src="${WORK_DIR}",dst=/src,type=bind \
   --mount src="${DATA_DIR}",dst=/src/data,type=bind \
   --mount src="$CACHE_DIR",dst=/root/.cache,type=bind \
   --mount src="$ALLENNLP_DIR",dst=/root/.allennlp,type=bind \
   --mount src="$NLTK_DATA",dst=/root/nltk_data,type=bind \
-  -e NVIDIA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
+  -e NVIDIA_VISIBLE_DEVICES="$CUDA_VISIBLE_DEVICES" \
   -w /src visualjoyce/andushu:latest \
   bash -c "bash ${RUN_TYPE}_${PROJECT}.sh ${MODEL_DIR}"
--- docker_train.sh
+++ docker_train.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 # Copyright (c) VisualJoyce.
 # Licensed under the MIT license.
 WORK_DIR=$(readlink -f .)
-DATA_DIR=$(readlink -f ${WORK_DIR}/data)
+DATA_DIR=$(readlink -f "${WORK_DIR}"/data)
 CACHE_DIR=${DATA_DIR}/.cache
 ALLENNLP_DIR=${DATA_DIR}/.allennlp
 NLTK_DATA=${DATA_DIR}/nltk_data
@@ -12,12 +13,12 @@ CONFIG=$3
 SPACY_LANGUAGE=$4
 MODEL_NAME=$5
 
-docker run --gpus '"'device=$CUDA_VISIBLE_DEVICES'"' --ipc=host --rm -it \
+docker run --gpus '"'device="$CUDA_VISIBLE_DEVICES"'"' --ipc=host --rm -it \
   --mount src="${WORK_DIR}",dst=/src,type=bind \
   --mount src="${DATA_DIR}",dst=/src/data,type=bind \
   --mount src="$CACHE_DIR",dst=/root/.cache,type=bind \
   --mount src="$ALLENNLP_DIR",dst=/root/.allennlp,type=bind \
   --mount src="$NLTK_DATA",dst=/root/nltk_data,type=bind \
-  -e NVIDIA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
+  -e NVIDIA_VISIBLE_DEVICES="$CUDA_VISIBLE_DEVICES" \
   -w /src visualjoyce/andushu:latest \
   bash -c "bash train_${PROJECT}.sh ${SUB_PROJECT} ${CONFIG} ${SPACY_LANGUAGE} ${MODEL_NAME}"
