24a447670c7b98d73104d29d93197c00b413955a
TAN Minghuan <vimostan@gmail.com>
2021-12-10 22:07:57 +0800

    mathxling will combine math23k and mathqa

--- docker_train.sh
+++ docker_train.sh
@@ -7,12 +7,6 @@ CACHE_DIR=${DATA_DIR}/.cache
 ALLENNLP_DIR=${DATA_DIR}/.allennlp
 NLTK_DATA=${DATA_DIR}/nltk_data
 
-#PROJECT=$1
-#SUB_PROJECT=$2
-#CONFIG=$3
-#SPACY_LANGUAGE=$4
-#MODEL_NAME=$5
-
 docker run --gpus '"'device="$CUDA_VISIBLE_DEVICES"'"' --ipc=host --rm -it \
   --mount src="${WORK_DIR}",dst=/src,type=bind \
   --mount src="${DATA_DIR}",dst=/src/data,type=bind \
@@ -21,4 +15,4 @@ docker run --gpus '"'device="$CUDA_VISIBLE_DEVICES"'"' --ipc=host --rm -it \
   --mount src="$NLTK_DATA",dst=/root/nltk_data,type=bind \
   -e NVIDIA_VISIBLE_DEVICES="$CUDA_VISIBLE_DEVICES" \
   -w /src visualjoyce/andushu:latest \
-  bash -c "PROJECT=${SUB_PROJECT} CONFIG=${CONFIG} SPACY_LANGUAGE=${SPACY_LANGUAGE} MODEL_NAME=${MODEL_NAME} bash train_${PROJECT}.sh "
+  bash -c "PROJECT=${SUB_PROJECT} CONFIG=${CONFIG} SPACY_LANGUAGE=${SPACY_LANGUAGE} MODEL_NAME=${MODEL_NAME} OP_TYPE=$OP_TYPE bash train_${PROJECT}.sh "
