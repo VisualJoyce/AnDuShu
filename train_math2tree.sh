ae02db4ecbe8f939810ba479dab64abfdc19ba21
TAN Minghuan <vimostan@gmail.com>
2021-12-13 21:54:53 +0800

    update world size constraint for shards

--- train_math2tree.sh
+++ train_math2tree.sh
@@ -3,14 +3,13 @@
 WORK_DIR=$(readlink -f .)
 DATA_DIR=$(readlink -f "${WORK_DIR}"/data)
 CONFIG_DIR=$(readlink -f "${WORK_DIR}"/configs)
-
+OUTPUT_DIR=data/output/"${PROJECT}"/"${CONFIG}"_"${SPACY_LANGUAGE}"_"${OP_TYPE}"_${MODEL_NAME}
 #PROJECT=$1
 #CONFIG=$2
 #SPACY_LANGUAGE=$3
-#MODEL_NAME=$4
 
 CUDA_VISIBLE_DEVICES=0,1 PYTHONPATH=src ANNOTATION_DIR=${DATA_DIR}/annotations/ \
   MODEL_NAME=${MODEL_NAME} TOKENIZERS_PARALLELISM=false POS_TAGS=false LANGUAGE="${SPACY_LANGUAGE}" OP_TYPE=${OP_TYPE} \
   allennlp train "${CONFIG_DIR}"/"${PROJECT}"/seq2seq_with_copy/"${CONFIG}".jsonnet \
-  -s data/output/"${PROJECT}"/"${CONFIG}"_"${SPACY_LANGUAGE}"_"${OP_TYPE}" \
+  -s "${OUTPUT_DIR}" \
   --include-package andushu
